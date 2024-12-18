82ee26a0007385721a7e5d50c7c948f2285ec87d
Caryen Song <caryensong@googlemai.com>
2024-12-18 17:39:47 +0100

    add style for basket and nav

---
+++ script/db.js
@@ -0,0 +1,42 @@
+let myDishes = [
+   {
+      "name" : "Spagetti Napoli",
+      "price": 7.50,
+      "descirption" : "Pasta mit Tomatensauce"
+   },
+   {
+      "name" : "Spagetti Cabonara",
+      "price": 9.50,
+      "descirption" : "mit Form-Truthahnschinken, Ei und Sahnesauce"
+   },
+   {
+      "name" : "Vegetarische Lasange",
+      "price": 10.50,
+      "descirption" : "Herzhafitge Lasagne mit Schichten aus Gemüse, Tomatensoße und mit Käse überbacken"
+   },
+   {
+      "name" : "Spagetti Pesto",
+      "price": 9.50,
+      "descirption" : "Pasta mit Spinat Pesto"
+   },
+   {
+      "name" : "Pizza Margherita",
+      "price": 7.50,
+      "descirption" : " mit Tomatensauce und Mozzarella"
+   },
+   {
+      "name" : "Pizza Funghi",
+      "price": 8.50,
+      "descirption" : "mit Champignons"
+   },
+   {
+      "name" : "Pizza Spinaci",
+      "price": 8.00,
+      "descirption" : "mit Spinat und Knoblauch"
+   },
+   {
+      "name" : "Pizza Italia",
+      "price": 9.00,
+      "descirption" : "mit Spinat, Zwiebeln und Paprika"
+   },
+   ]
