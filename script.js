fd2295e3dd68de4042eeb57b6d9c65cb58b5cf4e
Caryen Song <caryensong@googlemai.com>
2024-12-19 22:03:37 +0100

    add templates.js

--- script.js
+++ script.js
@@ -15,27 +15,6 @@ function render() {
    }
 }
 
-function getHeadlineTemplate(i) {
-   return `
-      <div class="category">
-         <img class="header_img" src="./assets/img/${myDishesHeadline[i].img}" />
-         <h3>${myDishesHeadline[i].categoryName}</h3>
-      </div>`;
-}
-
-
-function getDishTemplate(dish) {
-   return `
-      <div class="order_list">
-         <img class="add_icon" src="./assets/icon/add.png" />
-         <div class="dish_info">
-            <h4>${dish.name}</h4>
-            <p>${dish.descirption}</p>
-            <p class="price">${dish.price.toFixed(2)} Euro</p>
-         </div>
-      </div>`;
-}
-
 function getDishesForCategory(category){
    const categoryMapping = {
       "Nudelgerichte": ["Spagetti Napoli", "Spagetti Cabonara", "Vegetarische Lasange"],
