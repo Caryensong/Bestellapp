24cd84d89507b7b6801c8b29bbb02547f20c29de
Caryensong <caryensong@googlemail.com>
2024-12-19 12:27:37 +0100

    addd for loop for order List

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function init(){
-   
+render();
+}
+
+
+function render() {
+   let contentOrderRef = document.getElementById("orderlistContentPasta");
+   contentOrderRef.innerHTML = "";
+
+   for(let i = 0; i < myDishes.length; i++){
+      contentOrderRef.innerHTML += getOrderlistContentTemplate(i);
+   }
+}
+
+function getOrderlistContentTemplate(i){
+   return `   <div id="orderlistContentPizza" class="order_list" >
+   <h4>${myDishes[i].name}</h4>
+              <p>${myDishes[i].descirption}</p>
+              <p class="price">${myDishes[i].price.toFixed(2)} €</p>
+              </div>
+   `
 }
