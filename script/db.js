ae0ecd7222e91e07e5efff978cd6692b1403af46
Caryensong <caryensong@googlemail.com>
2024-12-23 16:08:00 +0100

    add amount

--- script/db.js
+++ script/db.js
@@ -2,50 +2,59 @@ let myDishes = [
    {
       "name" : "Spagetti Napoli",
       "price": 7.50,
-      "description" : "Pasta mit Tomatensauce"
+      "description" : "Pasta mit Tomatensauce",
+      "amount": 1
    },
    {
       "name" : "Spagetti Cabonara",
       "price": 9.50,
-      "description" : "mit Form-Truthahnschinken, Ei und Sahnesauce"
+      "description" : "mit Form-Truthahnschinken, Ei und Sahnesauce","amount": 1
    },
    {
       "name" : "Vegetarische Lasange",
       "price": 10.50,
-      "description" : "Herzhafitge Lasagne mit Schichten aus Gemüse, Tomatensoße und mit Käse überbacken"
+      "description" : "Herzhafitge Lasagne mit Schichten aus Gemüse, Tomatensoße und mit Käse überbacken",
+      "amount": 1
    },
    {
       "name" : "Pizza Margherita",
       "price": 7.50,
-      "description" : " mit Tomatensauce und Mozzarella"
+      "description" : " mit Tomatensauce und Mozzarella",
+      "amount": 1
    },
    {
       "name" : "Pizza Funghi",
       "price": 8.50,
-      "description" : "mit Champignons"
+      "description" : "mit Champignons",
+      "amount": 1
    },
    {
       "name" : "Pizza Spinaci",
       "price": 8.00,
-      "description" : "mit Spinat und Knoblauch"
+      "description" : "mit Spinat und Knoblauch",
+      "amount": 1
    },
    {
       "name" : "Pizza Italia",
       "price": 9.00,
-      "description" : "mit Spinat, Zwiebeln und Paprika"
+      "description" : "mit Spinat, Zwiebeln und Paprika",
+      "amount": 1
    },
    {
       "name" : "Hausgemachte Limonade",
       "price": 4.50,
-      "description" : "mit Zitrone und Minze"
+      "description" : "mit Zitrone und Minze",
+      "amount": 1
    },{
       "name" : "Himbeer-Limonade",
       "price": 4.50,
-      "description" : "mit Rosmarin, Limette und Himbeeren"
+      "description" : "mit Rosmarin, Limette und Himbeeren",
+      "amount": 1
    },{
       "name" : "Rote-Bete-Limonade",
       "price": 4.80,
-      "description" : "mit Zitronensaft und Rote Bete,"
+      "description" : "mit Zitronensaft und Rote Bete,",
+      "amount": 1
    },
    ];
 
