0a080756614386052adb4f1c35fced69123617c4
Caryensong <caryensong@googlemail.com>
2024-12-26 16:55:04 +0100

    add windows scroll navLink class activ

--- script/respo.js
+++ script/respo.js
@@ -1,8 +1,8 @@
 const navbar = document.getElementById('navbar');
+const stickyPoint = navbar.offsetTop; // Position der Navbar speichern
+
 
 if (navbar) {
-  const stickyPoint = navbar.offsetTop; // Position der Navbar speichern
-
   window.onscroll = function () {
     if (window.scrollY >= stickyPoint) {
       navbar.classList.add('sticky'); // Sticky aktivieren
@@ -11,3 +11,27 @@ if (navbar) {
     }
   };
 } 
+
+function positionScreen() {
+   const categoryPasta = document.getElementById('categoryList0'); 
+   const categoryPizza = document.getElementById('categoryList1'); 
+   const categoryDrinks = document.getElementById('categoryList2'); 
+   const navLinkPasta = document.getElementById('linkPasta');
+   const navLinkPizza = document.getElementById('linkPizza');
+   const navLinkDrinks = document.getElementById('linkDrinks');
+   
+   const navLinks = [navLinkPasta, navLinkPizza, navLinkDrinks];
+   navLinks.forEach(link => link.classList.remove('active'));
+   
+   const scrollPosition = window.scrollY + window.innerHeight; 
+
+   if (scrollPosition >= categoryDrinks.offsetTop + 500) {
+       navLinkDrinks.classList.add('active');
+   } else if (scrollPosition >= categoryPizza.offsetTop + 700) {
+       navLinkPizza.classList.add('active');
+   } else if (scrollPosition >= categoryPasta.offsetTop) {
+       navLinkPasta.classList.add('active');
+   }
+}
+
+window.addEventListener('scroll', positionScreen);
